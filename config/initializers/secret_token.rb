0ee8e495f83948ab428f9829014a502f017ba4d2
Ryoji Miyazaki <ryoji.miyazaki@sonymobile.com>
2014-07-03 15:14:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AttendeesWizard::Application.config.secret_key_base = '4655032026d559e9ecf78c8f1892f1ea0026c565a9f86a09986ae62cc42aaef59a392f67f60f623dbc2d1e5895c9eb2e5cb4f752d60353c55ea4fceb8c523b89'
