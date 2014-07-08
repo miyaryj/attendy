349190e64d5e55f5d6bc46b011853e77e87e998c
miyaryj <miya.ryj@gmail.com>
2014-07-08 18:04:29 +0900

    Introduce global constants for TITLE and DATE

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 AttendeesWizard::Application.initialize!
+
+EVENT_TITLE = "Movies & VU President's Award Party"
+EVENT_DATE = "8/8(Fri)"
